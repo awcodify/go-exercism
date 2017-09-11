3fe74dc7608195876be5ef79fafd17ee01b08526
Danu Akbar <awcodify@gmail.com>
2017-09-11 17:03:18 +0700

    [raindrops] just use if condition instead of array

--- 7.raindrops/raindrops.go
+++ 7.raindrops/raindrops.go
@@ -5,16 +5,20 @@ import "fmt"
 const testVersion = 3
 
 func Convert(num int) string {
-	rain := make([]string, 10)
-	rain[3] = "Pling"
-	rain[5] = "Plang"
-	rain[7] = "Plong"
 	sounds := ""
-	for i, sound := range rain {
-		if 0 < i && num%i == 0 {
-			sounds += sound
-		}
+
+	if num%3 == 0 {
+		sounds += "Pling"
 	}
+
+	if num%5 == 0 {
+		sounds += "Plang"
+	}
+
+	if num%7 == 0 {
+		sounds += "Plong"
+	}
+
 	if len(sounds) == 0 {
 		sounds = fmt.Sprintf("%d", num)
 	}
